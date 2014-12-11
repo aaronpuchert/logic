24f063339b0e4a63c803ed88a0257bfd4e1660fb
Aaron Puchert <aaronpuchert@alice-dsl.net>
2014-12-11 19:52:55 +0100

    Wrote test along the lines of examples/simple.lth

--- test/write.cpp
+++ test/write.cpp
@@ -1,4 +1,5 @@
 #include "../core/logic.hpp"
+#include "../core/theory.hpp"
 #include "../core/lisp.hpp"
 #define BOOST_TEST_MODULE LispTest
 #define BOOST_TEST_DYN_LINK
@@ -8,11 +9,12 @@
 using namespace Core;
 using std::make_shared;
 
-void checkRule(Rule_ptr rule, const std::string& result)
+template <typename T>
+void checkResult(const T *object, const std::string& result)
 {
 	std::ostringstream stream;
 	Writer writer(stream);
-	rule->accept(&writer);
+	object->accept(&writer);
 	BOOST_CHECK_EQUAL(stream.str(), result);
 }
 
@@ -31,14 +33,14 @@ BOOST_AUTO_TEST_CASE(rule_writer_test)
 	Rule_ptr tautology = make_shared<Tautology>("excluded_middle",
 		Rule::VarList{*stmt_a}, taut_stmt);
 
-	checkRule(tautology, "(tautology excluded_middle (list (statement a)) (or a (not a)))\n");
+	checkResult(tautology.get(), "(tautology excluded_middle (list (statement a)) (or a (not a)))\n");
 
 	// Rule of double negation.
 	Expr_ptr not_not_a = make_shared<NegationExpr>(not_a);
 	Rule_ptr equivrule = make_shared<EquivalenceRule>("double_negation",
 		Rule::VarList{*stmt_a}, not_not_a, expr_a);
 
-	checkRule(equivrule, "(equivrule double_negation (list (statement a)) (not (not a)) a)\n");
+	checkResult(equivrule.get(), "(equivrule double_negation (list (statement a)) (not (not a)) a)\n");
 
 	// The modus ponens rule.
 	Expr_ptr impl = make_shared<ConnectiveExpr>(ConnectiveExpr::IMPL, expr_a, expr_b);
@@ -46,5 +48,67 @@ BOOST_AUTO_TEST_CASE(rule_writer_test)
 	Rule_ptr deductionrule = make_shared<DeductionRule>("ponens",
 		Rule::VarList{*stmt_a, *stmt_b}, premisses, expr_b);
 
-	checkRule(deductionrule, "(deductionrule ponens (list (statement a) (statement b)) (list (impl a b) a) b)\n");
+	checkResult(deductionrule.get(), "(deductionrule ponens (list (statement a) (statement b)) (list (impl a b) a) b)\n");
+}
+
+BOOST_AUTO_TEST_CASE(theory_writer_test)
+{
+	// (type person)
+	Type_ptr person = make_shared<Type>("person");
+	checkResult(person.get(), "(type person)\n");
+	// (predicate schüler? (list person))
+	std::shared_ptr<PredicateDecl> student = make_shared<PredicateDecl>("schüler?", 1);
+	student->setParameterType(0, person);
+	checkResult(student.get(), "(predicate schüler? (list person))\n");
+	// (predicate dumm? (list person))
+	std::shared_ptr<PredicateDecl> stupid = make_shared<PredicateDecl>("dumm?", 1);
+	stupid->setParameterType(0, person);
+	checkResult(stupid.get(), "(predicate dumm? (list person))\n");
+
+	// (person fritz) ; this is in fact a constant, but what is the difference?
+	Var_ptr fritz = make_shared<Variable>(person, "fritz");
+	checkResult(fritz.get(), "(person fritz)\n");
+	Expr_ptr fritz_expr = make_shared<AtomicExpr>(fritz);
+	// (axiom (schüler? fritz))
+	Expr_ptr axiom1_expr = make_shared<PredicateExpr>(student,
+		std::vector<Expr_ptr>{fritz_expr});
+	Statement axiom1("fritz_is_student", axiom1_expr);
+	checkResult(&axiom1, "(axiom (schüler? fritz))\n");
+
+	// (axiom (forall (list (person x)) (impl (schüler? x) (dumm? x))))
+	Var_ptr var_x = make_shared<Variable>(person, "x");
+	Expr_ptr expr_x = make_shared<AtomicExpr>(var_x);
+	Expr_ptr student_x = make_shared<PredicateExpr>(student,
+		std::vector<Expr_ptr>{expr_x});
+	Expr_ptr stupid_x = make_shared<PredicateExpr>(stupid,
+		std::vector<Expr_ptr>{expr_x});
+	Expr_ptr impl = make_shared<ConnectiveExpr>(ConnectiveExpr::IMPL,
+		student_x, stupid_x);
+	PredicateLambda impl_pred(std::vector<Variable>{*var_x}, impl);
+	Expr_ptr forall_expr = make_shared<QuantifierExpr>
+		(QuantifierExpr::FORALL, std::move(impl_pred));
+	Statement axiom2("students_are_stupid", forall_expr);
+	checkResult(&axiom2, "(axiom (forall (list (person x)) (impl (schüler? x) (dumm? x))))\n");
+
+	//  (statement
+	//  	(dumm? fritz)
+	Expr_ptr statement_expr = make_shared<PredicateExpr>(stupid,
+		std::vector<Expr_ptr>{fritz_expr});
+	Statement statement("fritz_is_stupid", statement_expr);
+	//  	(proof
+	//  		(statement (impl (schüler? fritz) (dumm? fritz))
+	//  			(specialization
+	//  				(list (predicate (list (person x)) (impl (schüler? x) (dumm? x))) person fritz)
+	//  				(list (ref parent~1))
+	//  			)
+	//  		)
+	//  		(statement (dumm? fritz)
+	//  			(ponens
+	//  				(list (schüler? fritz) (dumm? fritz))
+	//  				(list (ref parent~2) (ref this~1))
+	//  			)
+	//  		)
+	//  	)
+	//  )
+	// TODO: add the proof
 }
